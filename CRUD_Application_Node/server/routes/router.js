d766fe93edfe997773fc7beb91e054993734d3b6
AhsanTariqMajeed <93177690+AhsanTariqMajeed@users.noreply.github.com>
2022-05-27 10:06:28 +0500

    view crud

---
+++ CRUD_Application_Node/server/routes/router.js
@@ -0,0 +1,182 @@
+const express = require('express');
+const route = express.Router()
+const User = require('../model/model.js');
+
+const services = require('../services/render');
+const controller = require('../controller/controller');
+
+
+route.get('/reg', (req, res, next) => {
+	return res.render('home.ejs');
+});
+
+
+route.post('/reg', (req, res, next) => {
+	let personInfo = req.body;
+
+	if (!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf) {
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
+
+			User.findOne({ email: personInfo.email }, (err, data) => {
+				if (!data) {
+					let c;
+					User.findOne({}, (err, data) => {
+
+						if (data) {
+							c = data.unique_id + 1;
+						} else {
+							c = 1;
+						}
+
+						let newPerson = new User({
+							unique_id: c,
+							email: personInfo.email,
+							username: personInfo.username,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf
+						});
+
+						newPerson.save((err, Person) => {
+							if (err)
+								console.log(err);
+							else
+								console.log('Success');
+						});
+
+					}).sort({ _id: -1 }).limit(1);
+					res.send({ "Success": "You are regestered,You can login now." });
+				} else {
+					res.send({ "Success": "Email is already used." });
+				}
+
+			});
+		} else {
+			res.send({ "Success": "password is not matched" });
+		}
+	}
+});
+
+route.get('/login', (req, res, next) => {
+	return res.render('login.ejs');
+});
+
+route.post('/login', (req, res, next) => {
+	User.findOne({ email: req.body.email }, (err, data) => {
+		if (data) {
+
+			if (data.password == req.body.password) {
+				req.session.userId = data.unique_id;
+				res.send({ "Success": "Success!" });
+			} else {
+				res.send({ "Success": "Wrong password!" });
+			}
+		} else {
+			res.send({ "Success": "This Email Is not regestered!" });
+		}
+	});
+});
+
+route.get('/profile', (req, res, next) => {
+	User.findOne({ unique_id: req.session.userId }, (err, data) => {
+		if (!data) {
+			res.redirect('/');
+		} else {
+			return res.render('data.ejs', { "name": data.username, "email": data.email });
+		}
+	});
+});
+
+
+route.get('/logout', (req, res, next) => {
+	if (req.session) {
+		// delete session object
+		req.session.destroy((err) => {
+			if (err) {
+				return next(err);
+			} else {
+				return res.redirect('/');
+			}
+		});
+	}
+});
+
+
+
+
+
+route.get('/EditProfile', (req, res, next) => {
+	return res.render('EditProfile.ejs');
+});
+
+route.get('/EditProfile', (req, res, next) => {
+	if (req.session) {
+		// delete session object
+		req.session.destroy((err) => {
+			if (err) {
+				return next(err);
+			} else {
+				return res.redirect('/');
+			}
+		});
+	}
+});
+
+
+route.get('/forgetpass', (req, res, next) => {
+	res.render("forget.ejs");
+});
+
+route.post('/forgetpass', (req, res, next) => {
+	User.findOne({ email: req.body.email }, (err, data) => {
+		if (!data) {
+			res.send({ "Success": "This Email Is not regestered!" });
+		} else {
+			if (req.body.password == req.body.passwordConf) {
+				data.password = req.body.password;
+				data.passwordConf = req.body.passwordConf;
+
+				data.save((err, Person) => {
+					if (err)
+						console.log(err);
+					else
+						console.log('Success');
+					res.send({ "Success": "Password changed!" });
+				});
+			} else {
+				res.send({ "Success": "Password does not matched! Both Password should be same." });
+			}
+		}
+	});
+
+});
+
+/**
+ *  @description Root Route
+ *  @method GET /
+ */
+route.get('/', services.homeRoutes);
+
+/**
+ *  @description add users
+ *  @method GET /add-user
+ */
+route.get('/add-user', services.add_user)
+
+/**
+ *  @description for update user
+ *  @method GET /update-user
+ */
+route.get('/update-user', services.update_user)
+
+
+// API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
+
+
+module.exports = route
+
